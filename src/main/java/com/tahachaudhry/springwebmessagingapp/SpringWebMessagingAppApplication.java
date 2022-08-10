c96372f244a75fa93e533b591eb72231fd81f91d
Taha Chaudhry <tahachaudhry@Tahas-MacBook-Air.local>
2022-08-11 01:23:19 +0300

    first commit

---
+++ src/main/java/com/tahachaudhry/springwebmessagingapp/SpringWebMessagingAppApplication.java
@@ -0,0 +1,13 @@
+package com.tahachaudhry.springwebmessagingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMessagingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMessagingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tahachaudhry/springwebmessagingapp/SpringWebMessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tahachaudhry.springwebmessagingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMessagingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
